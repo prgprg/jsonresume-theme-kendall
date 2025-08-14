608cd3955b68970da6159833eaddd10fc0279a67
apourgho <ali.pourgholami@inform-software.com>
2025-08-14 12:33:33 +0200

    docs removed

--- docs/index.js
+++
@@ -1,8 +0,0 @@
-// Placeholder client script; fetch and render resume if desired
-fetch('./resume.json')
-  .then(r => r.json())
-  .then(data => {
-    // You can integrate a client-side render here if needed.
-    console.log('Resume loaded', data.basics?.name);
-  })
-  .catch(err => console.error('Failed to load resume.json', err));
